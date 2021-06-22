b2f8e9d2d8859dd187fc4927252cd57d6a5024c3
Ryland Oehlers <ryland.oehlers@gmail.com>
2021-06-22 11:47:10 -0400

    new quote function added and tweet btn

--- src/App.js
+++ src/App.js
@@ -20,6 +20,17 @@ function App () {
 		}
 	};
 
+	const getRandomQuote = () => {
+		try {
+			axios.get('/random').then((res) => {
+				const q = res.data;
+				setQuote(q);
+			});
+		} catch (e) {
+			console.log(e);
+		}
+	};
+
 	return (
 		<div className="App">
 			<Nav />
@@ -29,13 +40,25 @@ function App () {
 						<div className="quote-container" key={pos}>
 							<div className="quote">"{q.q}"</div>
 							<div className="author">- {q.a}</div>
-							<a href="#">
-								<i className="far fa-plus-square" />
-							</a>
+							<div className="btn-group">
+								<button className="new-btn" onClick={getRandomQuote}>
+									New Quote
+								</button>
+								<a
+									className="twitter-btn"
+									href={`https://twitter.com/intent/tweet?text=${q.q}${'  - '}${q.a}`}
+									target="_blank"
+									rel="noopener noreferrer"
+								>
+									<i className="fab fa-twitter" />
+								</a>
+							</div>
 						</div>
 					))
 				) : (
-					<button onClick={getQuote}>Get Quote of the Day</button>
+					<button className="get-btn" onClick={getQuote}>
+						Get Quote of the Day
+					</button>
 				)}
 			</div>
 		</div>
--- src/Nav.js
+++ src/Nav.js
@@ -9,11 +9,6 @@ const Nav = () => {
 						<i className="fas fa-quote-right" />
 					</a>
 				</li>
-				<li>
-					<a href="/yours">
-						<i class="fas fa-bookmark" />
-					</a>
-				</li>
 			</ul>
 		</nav>
 	);
